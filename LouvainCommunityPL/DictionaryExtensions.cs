619aeb8e576d371e4a15159d6f954fe9bd22ccf1
Andreas Grünwald <andreasgruenwald@live.de>
2016-08-11 19:13:13 +0200

    Add DictionaryExtensions

---
+++ LouvainCommunityPL/DictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LouvainCommunityPL
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dicitionary, TKey key, TValue defaultValue)
+        {
+            return dicitionary.ContainsKey(key) ? dicitionary[key] : defaultValue;
+        }
+    }
+}
